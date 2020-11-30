c1ec52115a6425d724c4d5e2ea8105067391ff6d
heltonr13 <50843386+heltonr13@users.noreply.github.com>
2020-11-30 14:57:12 -0300

    Update ClienteController.java

--- src/main/java/io/github/heltonricardo/exerciciossb/controllers/ClienteController.java
+++ src/main/java/io/github/heltonricardo/exerciciossb/controllers/ClienteController.java
@@ -1,6 +1,7 @@
 package io.github.heltonricardo.exerciciossb.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +13,12 @@ import io.github.heltonricardo.exerciciossb.models.Cliente;
  * obterCliente, por exemplo, será localhost:8080/clientes/qualquer. Quando a
  * anotação tem somente a propriedade "path", é possível omitir o nome dela,
  * como feito em obterCliente().
+ * 
+ * O método obterClientePorId1() possui uma URL com parâmetro. Nesse caso a URL
+ * completa será: "localhost:8080/clientes/id" sendo "id" um valor inteiro e
+ * com o mesmo nome do parâmetro da assinatura do método. A variável do
+ * parâmetro da função deve possuir a anotação PathVariable
+ * 
  */
 
 @RestController
@@ -21,5 +28,10 @@ public class ClienteController {
 	@GetMapping("/qualquer")
 	public Cliente obterCliente() {
 		return new Cliente(13, "Helton", "123.456.789-09");
+	}
+	
+	@GetMapping("/{id}")
+	public Cliente obterClientePorId1(@PathVariable int id) {
+		return new Cliente(id, "Maria", "321.654.987.98");
 	}
 }
