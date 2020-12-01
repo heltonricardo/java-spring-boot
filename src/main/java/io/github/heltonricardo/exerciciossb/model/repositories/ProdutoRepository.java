7cdc8f00efabe0c678bbfcfc9e9b77d72c90350b
heltonr13 <50843386+heltonr13@users.noreply.github.com>
2020-12-01 11:49:02 -0300

    Create ProdutoRepository.java

---
+++ src/main/java/io/github/heltonricardo/exerciciossb/model/repositories/ProdutoRepository.java
@@ -0,0 +1,23 @@
+package io.github.heltonricardo.exerciciossb.model.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import io.github.heltonricardo.exerciciossb.model.entities.Produto;
+
+/* 
+ * Um repositório pode ser um arquivo texto, uma planilha, um banco de dados
+ * etc. O padrão Repository tem a ideia de criar métodos de alto nível que irão
+ * abstrair todo acesso aos dados. Semelhante ao DAO que é mais voltado para a
+ * implementação, o Repository se preocupa mais em criar as interfaces para
+ * abstração.
+ * 
+ * Essa interface é responsável por fazer a persistência de um produto.
+ * Ela extende outra interface que já possui implementações necessárias por
+ * fazer essa persistência. Nos generics de CrudRepository inserimos a classe
+ * dos objetos que serão persistidos e o tipo Wrapper do identificador. Como em
+ * produto usando "int", aqui usaremos "Integer".
+ */
+
+public interface ProdutoRepository extends CrudRepository<Produto, Integer> {
+
+}
