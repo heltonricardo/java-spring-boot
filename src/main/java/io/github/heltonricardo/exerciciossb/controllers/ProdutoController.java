fad7b39c7e1183d2c82d5e156586d23d74ab8526
heltonr13 <50843386+heltonr13@users.noreply.github.com>
2020-12-02 10:57:29 -0300

    Update ProdutoController.java

--- src/main/java/io/github/heltonricardo/exerciciossb/controllers/ProdutoController.java
+++ src/main/java/io/github/heltonricardo/exerciciossb/controllers/ProdutoController.java
@@ -11,6 +11,19 @@ import io.github.heltonricardo.exerciciossb.model.entities.Produto;
 import io.github.heltonricardo.exerciciossb.model.repositories.ProdutoRepository;
 
 /*
+ * Autowired é usada quando fazemos o que é chamado de Dependence Injection
+ * (Injeção de Dependência) que é um tipo de Inversion of Control (Inversão de
+ * Controle). Quando uma classe A depende de uma classe B, normalmente
+ * instanciamos um objeto de B em A. Porém, se a classe A está sob controle de
+ * um framework, podemos deixar também sob responsabilidade do framework
+ * instanciar o objeto B dentro de A. Por isso o nome Inversão de Controle:
+ * as responsabilidades são invertidas para framework, que fará grande parte do
+ * trabalho. Pode-se notar que em nenhum momento instanciamos a classe
+ * ProdutoController, pois ela está anotada com @RestController e isso faz com
+ * que ela seja agora responsabilidade de um framework. Todas os controllers
+ * foram instanciados e controlados automaticamente por esse mesmo framework,
+ * que nesse caso é o Spring Boot.
+ * 
  * A anotação ResponseBody não é necessária. Serve somente para deixar claro
  * que o Produto retornado faz parte do corpo da resposta.
  */
