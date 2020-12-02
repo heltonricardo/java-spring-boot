f63737fa24e53a8873158eab929ed620e0968385
heltonr13 <50843386+heltonr13@users.noreply.github.com>
2020-12-02 14:24:52 -0300

    Update Produto.java

--- src/main/java/io/github/heltonricardo/exerciciossb/model/entities/Produto.java
+++ src/main/java/io/github/heltonricardo/exerciciossb/model/entities/Produto.java
@@ -4,6 +4,17 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+
+/*
+ * Adicionamos algumas regras para validação de dados em alguns atributos.
+ * NotBlank: não vazio; Min: valor mímino; Max: valor máximo. Para ser possível
+ * usar essas anotações, é necessário adicionar a dependência Hibernate
+ * Validator. Saiba como fazê-lo no final do arquivo INSTRUÇÔES.md, presente
+ * nesse projeto.
+ */
 
 @Entity
 public class Produto {
@@ -11,15 +22,16 @@ public class Produto {
 	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	
+	@NotBlank
 	private String nome;
 	
+	@Min(0)
 	private double preco;
 	
+	@Min(0) @Max(1)
 	private double desconto;
 	
-	public Produto() {
-		
-	}
+	public Produto() {}
 	
 	public Produto(String nome, double preco, double desconto) {
 		this.nome = nome;
