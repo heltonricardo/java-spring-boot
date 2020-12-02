ddbf44313fdca657730a583bc39e86b503070b8d
heltonr13 <50843386+heltonr13@users.noreply.github.com>
2020-12-02 11:17:16 -0300

    Update ProdutoController.java

--- src/main/java/io/github/heltonricardo/exerciciossb/controllers/ProdutoController.java
+++ src/main/java/io/github/heltonricardo/exerciciossb/controllers/ProdutoController.java
@@ -36,8 +36,12 @@ public class ProdutoController {
 	private ProdutoRepository produtoRepository;
 
 	@PostMapping
-	public @ResponseBody Produto novoProduto(@RequestParam String nome) {
-		Produto produto = new Produto(nome);
+	public @ResponseBody Produto novoProduto(
+		   @RequestParam String nome,
+		   @RequestParam double preco,
+		   @RequestParam double desconto) {
+		
+		Produto produto = new Produto(nome, preco, desconto);
 		produtoRepository.save(produto);
 		return produto;
 	}
