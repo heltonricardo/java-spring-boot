9f94872a3c58c906410592baaefe66456df39f25
Helton Ricardo <50843386+heltonr13@users.noreply.github.com>
2021-01-21 17:42:26 -0300

    Update ProdutoController.java

--- src/main/java/io/github/heltonricardo/exerciciossb/controllers/ProdutoController.java
+++ src/main/java/io/github/heltonricardo/exerciciossb/controllers/ProdutoController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -64,5 +65,11 @@ public class ProdutoController {
 	@GetMapping(path = "/{id}")
 	public Optional<Produto> obterProdutoPorId(@PathVariable int id) {
 		return produtoRepository.findById(id);
+	}
+	
+	@PutMapping
+	public Produto alterarProduto(@Valid Produto produto) {
+		produtoRepository.save(produto);
+		return produto;
 	}
 }
